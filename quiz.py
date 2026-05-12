a9515a0d9000d0b98267c8a8c8c0dc83f253fd6a
farhananan <rahmanfa@wsc.school.nz>
2026-05-12 14:40:41 +1200

    made a quiz

---
+++ quiz.py
@@ -0,0 +1,26 @@
+# Ask user for a name and save it in a variable
+name = input("What is your name? ")
+print("My name is " + name)
+# Ask user for an animal and save it in a variable
+animal = input("What is your favorite animal? ")
+print("My favorite animal is " + animal)
+# Ask user for a colour and save it in a variable
+colour = input("What is your favorite colour? ")
+print("My favorite colour is " + colour)
+# Ask user for a place and save it in a variable
+place = input("What is your favorite place? ")
+print("My favorite place is " + place)
+# Ask user for a sport and save it in a variable
+sport = input("What is your favorite sport? ")
+print("My favorite sport is " + sport)
+# Ask user for a food and save it in a variable
+food = input("What is your favorite food? ")
+print("My favorite food is " + food)
+# Ask user for a favorite movie and save it in a variable
+movie = input("What is your favorite movie? ")
+print("My favorite movie is " + movie)
+# Ask user for  a favorite song and save it in a variable
+song = input("What is your favorite song? ")
+print("My favorite song is " + song)
+# Print your madlib using the 6 variables above.
+print(name + " loves " + animal + "s. " + name + "'s favorite color is " + colour + " and " + name + "'s favorite place is " + place + ". " + name + " also loves playing " + sport + " and eating " + food + ". Their favorite movie is " + movie + " and their favorite song is " + song + ".")
