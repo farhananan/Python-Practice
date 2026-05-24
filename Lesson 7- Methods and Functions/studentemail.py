2875cf4c6d417a034d7047fd36c7badcad57288b
farhananan <rahmanfa@wsc.school.nz>
2026-05-25 11:32:57 +1200

    made a student email creator with python

---
+++ Lesson 7- Methods and Functions/studentemail.py
@@ -0,0 +1,38 @@
+# Create a student email creator that uses first and last name plus id
+# eg. smithjohn123@fake.school.nz
+
+# Get input (first, last, id) and save in variables
+print ("Welcome to the student email creator! Please enter your first name:")
+first_name = input()
+print ("Please enter your last name:")
+last_name = input()
+print ("Please enter your ID:")
+id = input()
+
+# Strip input to remove accidental spaces and turn names into lowercase (resave in variables)
+first_name = first_name.strip().lower()
+last_name = last_name.strip().lower()
+id = id.strip()
+
+
+
+# Output the final email address
+print("Your student email is:", last_name + first_name + id + "@wsc.school.nz")
+
+
+# --------------------------------
+
+# EXTENSION
+# Create a temporary password to output as well
+# It should be their names in all uppercase and their id divided by 10
+
+# --------------------------------
+
+# EXPERT
+# Create a WSCW email creator
+# Get the users first and last name, then randomly generate an ID number (8 digits long)
+# Output the email addess (lastf.wsc.school.nz) 
+# - you'll need to strip down the first name to just first letter
+# Output their id number
+# Output a temporary password (all uppercase). You can choose how you create this, 
+# but it needs to be unique for each user
--- _main.py
+++ _main.py
@@ -1,1 +1,6 @@
-
+print ('Hello . upper()')
+print ('Hello . lower()')
+if 'HELLO' == 'hello':
+    print('The same!')
+if 'HELLO'.lower() == 'hello':
+    print('The same!')
